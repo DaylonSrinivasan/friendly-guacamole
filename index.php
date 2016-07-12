c9e36fd857c8adc6dcef1f6cd5b79a801b1ae956
DaylonSrinivasan <daylon.srinivasan@gmail.com>
2016-07-12 16:33:46 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 <html>
  <body>
   Hello world
+ <?php
+  $to      = 'daylon.srinivasan@gmail.com';
+  $subject = 'the subject';
+  $message = 'hello';
+  $headers = 'From: no-reply@example.com' . "\r\n" .
+    'Reply-To: dont@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  mail($to, $subject, $message, $headers);
+ ?>
  </body>
 </html>
